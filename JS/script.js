6dc9bd440b7dedeca49e00ab2aa259f5503b07d6
H4m4k <pawel.janik.1983@gmail.com>
2021-03-22 12:02:00 +0100

    added missing error alerts below input

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,18 @@
+let button = document.querySelector('#greenButton');
+button.addEventListener('click', validate);
+
+function validate() {
+    let form = document.querySelector('.form');
+    
+    let fName = form.children.first_Name.value ? '' : console.log('First name cannot be empty');
+    let lName = form.children.last_Name.value ? '' : console.log('Last name cannot be empty');
+    let email = 'Looks this is not an email';
+    let pass = form.children.password.value ? '' : console.log('Password cannot be empty');
+    
+    let array = [fName, lName, email, pass];
+
+}
+
 /*- Receive an error message when the `form` is submitted if:
   - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
   - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"* */
