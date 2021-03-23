a72c2b42cd2556244741557196b280ae4b6f211f
H4m4k <pawel.janik.1983@gmail.com>
2021-03-23 22:06:11 +0100

    added missing icons, sorted classes for icons

--- JS/script.js
+++ JS/script.js
@@ -13,6 +13,15 @@ function validate() {
 
 }
 
+/* Object validator will contain the true/false result of checks
+
+  validator {
+    fName = true/false,
+    lName = true/false,
+    email = true/false, 
+    pass = true/false
+  }
+*/
 /*- Receive an error message when the `form` is submitted if:
   - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
   - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"* */
