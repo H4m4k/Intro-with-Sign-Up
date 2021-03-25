d3db1e3514645786a3568409f89210b4b2dd4247
H4m4k <pawel.janik.1983@gmail.com>
2021-03-25 07:24:04 +0100

    form layout changed from flex to grid, js woking as intended

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,50 @@
 let button = document.querySelector('#greenButton');
-button.addEventListener('click', validate);
+button.addEventListener('click', check);
+let error_List = document.querySelectorAll('.error, .icon')
+let array = Array.from(error_List).sort(); 
 
-function validate() {
-    let form = document.querySelector('.form');
-    
-    let fName = form.children.first_Name.value ? '' : console.log('First name cannot be empty');
-    let lName = form.children.last_Name.value ? '' : console.log('Last name cannot be empty');
-    let email = 'Looks this is not an email';
-    let pass = form.children.password.value ? '' : console.log('Password cannot be empty');
-    
-    let array = [fName, lName, email, pass];
-
+function error(number){
+  array[number].classList.remove('off');
+  array[number+4].classList.remove('off');
 }
 
-/* Object validator will contain the true/false result of checks
+function no_Error(number){
+  array[number].classList.add('off');
+  array[number+4].classList.add('off');
+}
+
+function check() {
+  let input = document.querySelectorAll('input');
+  let input_Array = Array.from(input);
+
+  input_Array.forEach((element, index) => {
+    (!element.value) ? error(index) : no_Error(index);
+  })
+}
+
+
+// function validate() {
+//   let input = document.querySelectorAll('input');
+//   let input_Array = Array.from(input);
+//   const email = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    
+//     let validator = {
+//       fName: false, 
+//       lName: false, 
+//       email: false, 
+//       pass:  false } ;
+
+//      /*fName */ input_Array[0].value ? validator.fName = true : error(0);
+//      /*lName*/  input_Array[1].value ? validator.lName = true : error(1);
+//      /*email*/  input_Array[2].value.match(email) ? validator.email = true : error(2);
+//      /*pass*/   input_Array[3].value ? validator.pass = true : error(3);
+    
+    //  const check = Object.values(validator).every(bool => bool === true)
+    // console.log(validator + ' ' + check);
+//}
+
+/* Object validator will contain the true/false result of checks, 
+    on all checks true - submit 
 
   validator {
     fName = true/false,
@@ -24,4 +55,4 @@ function validate() {
 */
 /*- Receive an error message when the `form` is submitted if:
   - Any `input` field is empty. The message for this error should say *"[Field Name] cannot be empty"*
-  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"* */
+  - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Looks like this is not an email"*/
